73cda052efb307f1fa4e1754d46d174d91e1e76f
Alberto Pérez-Bermejo <alberto.perez-bermejo@bbva.com>
2017-07-29 11:46:37 +0200

    feat: add mockup and Unity Project

---
+++ Unity/lasttrain/Assets/Scripts/GameManager.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameManager : MonoBehaviour {
+
+	const string version = "0.0.1";
+
+	void Awake () {
+		Debug.Log("Last Train - version: " + version);
+	}
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
